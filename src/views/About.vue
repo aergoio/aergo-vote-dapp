00b1469e61b7a91542c8d8ac7e0902b01046f329
ImHun <load1999@gmail.com>
2023-04-04 16:36:30 +0900

    feat : update latest logo

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,48 +1,67 @@
 <template>
   <Vertical base="fill" class="vertical">
     <ViewTitle>Home</ViewTitle>
-      <slot></slot>
-      <Island>
-        <p>
-          <b>Welcome to the Aergo Voting portal!<br/></b>
-          This is a Dapp that allows you to view the current status of votes and submit your own votes.
-        </p>
+    <slot></slot>
+    <Island>
+      <p>
+        <b>Welcome to the Aergo Voting portal!<br /></b>
+        This is a Dapp that allows you to view the current status of votes and
+        submit your own votes.
+      </p>
 
-        <div class="portal-wrapper">
-          <a href="https://swap.aergo.io/">
-            <div class="card-top">
-              <img src="../assets/icon/aergo-token-swap.png" alt="aergo token swap">
+      <div class="portal-wrapper">
+        <a href="https://swap.aergo.io/">
+          <div class="card-top">
+            <div class="card-img-wrapper">
+              <img
+                class="card-img"
+                src="../assets/icon/aergo-token-swap.png"
+                alt="aergo token swap"
+              />
             </div>
-            <div class="card-bottom">
-              <img src="../assets/icon/token-swap-icon.svg" alt="">
-              <img src="../assets/icon/enter-arrow-btn.svg" alt="">
+          </div>
+          <div class="card-bottom">
+            <img src="../assets/icon/token-swap-icon.svg" alt="" />
+            <img src="../assets/icon/enter-arrow-btn.svg" alt="" />
+          </div>
+        </a>
+        <a href="https://aergoscan.io/">
+          <div class="card-top">
+            <div class="card-img-wrapper">
+              <img
+                class="card-img"
+                src="../assets/icon/aergo-scan.png"
+                alt="aergo scan"
+              />
             </div>
-          </a>
-          <a href="https://aergoscan.io/">
-            <div class="card-top">
-              <img src="../assets/icon/aergo-scan.png" alt="aergo scan">
+          </div>
+          <div class="card-bottom">
+            <img src="../assets/icon/scan-icon.svg" alt="" />
+            <img src="../assets/icon/enter-arrow-btn.svg" alt="" />
+          </div>
+        </a>
+        <a href="https://www.aergo.io/resources">
+          <div class="card-top">
+            <div class="card-img-wrapper">
+              <img
+                class="card-img"
+                src="../assets/icon/aergo-token-stats.png"
+                alt="aergo report"
+              />
             </div>
-            <div class="card-bottom">
-              <img src="../assets/icon/scan-icon.svg" alt="">
-              <img src="../assets/icon/enter-arrow-btn.svg" alt="">
-            </div>
-          </a>
-          <a href="https://www.aergo.io/resources">
-            <div class="card-top">
-              <img src="../assets/icon/aergo-report.png" alt="aergo report">
-            </div>
-            <div class="card-bottom">
-              <img src="../assets/icon/report-icon.svg" alt="">
-              <img src="../assets/icon/enter-arrow-btn.svg" alt="">
-            </div>
-          </a>
-        </div>
-      </Island>
+          </div>
+          <div class="card-bottom">
+            <img src="../assets/icon/report-icon.svg" alt="" />
+            <img src="../assets/icon/enter-arrow-btn.svg" alt="" />
+          </div>
+        </a>
+      </div>
+    </Island>
   </Vertical>
 </template>
 <script>
 import { Vertical } from '@aergoenterprise/lib-components/src/layout';
-import { Island} from '@aergoenterprise/lib-components/src/composite';
+import { Island } from '@aergoenterprise/lib-components/src/composite';
 import ViewTitle from '../components/ViewTitle';
 
 export default {
@@ -51,39 +70,47 @@ export default {
     Island,
     ViewTitle,
     Vertical
-  },
-}
+  }
+};
 </script>
 <style scoped lang="scss">
-  .portal-wrapper{
-    display: grid;
-    gap: 20px;
-    grid-auto-flow: row;
-    grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
-    grid-auto-rows: 250px;
-    -moz-box-align: start;
-    align-items: start;
-    padding: 10px;
+.portal-wrapper {
+  display: grid;
+  gap: 20px;
+  grid-auto-flow: row;
+  grid-template-columns: repeat(auto-fill, minmax(380px, 1fr));
+  grid-auto-rows: 250px;
+  -moz-box-align: start;
+  align-items: start;
+  padding: 10px;
 
-    a{
-      width: 100%;
-      height: 100%;
-      cursor: pointer;
-      border-radius: 5px;
-      box-shadow: 0 2px 7px 0 #d1d6e2;
-      box-sizing: border-box;
-      padding: 20px;
-      display: flex;
-      flex-flow: column;
-    }
-    .card-top{
-      flex: 1;
-    }
-    .card-bottom{
-      display: flex;
-      justify-content: space-between;
+  a {
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+    border-radius: 5px;
+    box-shadow: 0 2px 7px 0 #d1d6e2;
+    box-sizing: border-box;
+    padding: 20px;
+    display: flex;
+    flex-flow: column;
+  }
+  .card-top {
+    flex: 1;
+    .card-img-wrapper {
+      width: 210px;
+      .card-img {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        object-fit: cover;
+      }
     }
   }
-
-
+  .card-bottom {
+    display: flex;
+    justify-content: space-between;
+  }
+}
 </style>
